50f799211757adef80d73cdbe744c3429aaddb25
nitkanikita21 <waleryvvs1@gmail.com>
2023-08-04 23:37:49 +0200

    remove lombok @Getter in class

--- src/main/java/com/nitkanikita21/ecspaper/EcsPaperPlugin.java
+++ src/main/java/com/nitkanikita21/ecspaper/EcsPaperPlugin.java
@@ -3,12 +3,14 @@ package com.nitkanikita21.ecspaper;
 import com.nitkanikita21.ecspaper.items.ItemBundleApi;
 import com.nitkanikita21.ecspaper.paper.EventRegister;
 import com.nitkanikita21.ecspaper.testing.SayComponent;
-import lombok.Getter;
 import net.kyori.adventure.key.Key;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EcsPaperPlugin extends JavaPlugin {
-    @Getter
+    public static ItemBundleApi getItemBundleApi() {
+        return itemBundleApi;
+    }
+
     private static ItemBundleApi itemBundleApi;
 
     @Override
