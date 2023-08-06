f69e2c1abb72c8437fbdfb82edb205b8c2707e44
nitkanikita21 <waleryvvs1@gmail.com>
2023-08-06 02:30:24 +0200

    remove test components from main plugin

--- src/main/java/com/nitkanikita21/ecspaper/EcsPaperPlugin.java
+++ src/main/java/com/nitkanikita21/ecspaper/EcsPaperPlugin.java
@@ -1,11 +1,6 @@
 package com.nitkanikita21.ecspaper;
 
 import com.nitkanikita21.ecspaper.commons.EventRegister;
-import com.nitkanikita21.ecspaper.items.ItemBundle;
-import com.nitkanikita21.ecspaper.items.ItemBundleApi;
-import com.nitkanikita21.ecspaper.testing.DisplayComponent;
-import com.nitkanikita21.ecspaper.testing.SayComponent;
-import net.kyori.adventure.key.Key;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EcsPaperPlugin extends JavaPlugin {
@@ -13,21 +8,6 @@ public class EcsPaperPlugin extends JavaPlugin {
     public void onEnable() {
         ECS ecs = new ECS(new EventRegister(this));
 
-        ecs.getItemBundleApi().registerStaticComponent(
-                new SayComponent(Key.key("test", "say_my_name"))
-        );
-        ecs.getItemBundleApi().registerStaticComponent(
-                new DisplayComponent(Key.key("test", "display"))
-        );
-
-        ItemBundle testBundle = ecs.getItemBundleApi().createEmptyBundle();
-        testBundle.addComponent(new SayComponent(Key.key("test", "say_my_name")));
-        testBundle.addComponent(new DisplayComponent(Key.key("test", "display")));
-
-        ecs.getItemBundleApi().registerStaticBundle(
-                Key.key("test", "aboba"),
-                testBundle
-        );
     }
 
 
